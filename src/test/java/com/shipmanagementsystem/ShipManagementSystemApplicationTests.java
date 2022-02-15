3611bca82a49adb817418b4da45d4e5838419bd0
Oviya16 <oviyasateesh1608@gmail.com>
2022-02-15 18:34:20 +0530

    Created .

---
+++ src/test/java/com/shipmanagementsystem/ShipManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shipmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
